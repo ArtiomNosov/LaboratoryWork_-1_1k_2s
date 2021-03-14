a8cc6da4e8cf3152b7c8773c5cd8e2f8a66ab868
Носов Артём Иванович Б20-514 <artiom-nj@mail.ru>
2021-03-14 20:07:41 +0300

    После неудачной попытки, накнец у нас получилось сделать push.

---
+++ tests.h
@@ -0,0 +1,16 @@
+#pragma once
+//Модульные тесты на основыные алгоритмы.
+
+		/*Тестовые функции для map DynamicArray*/
+
+//Тест для map DynamicArray для значений типа int
+void testMapInDynamicArrayInt(void);
+
+//Тест для map DynamicArray для значений типа char
+void testMapInDynamicArrayChar(void);
+
+//Тест для map DynamicArray для значений типа float
+void testMapInDynamicArrayFloat(void);
+
+//Тест для map DynamicArray для значений типа function
+void testMapInDynamicArrayFunction(void);
